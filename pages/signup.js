c2e874be403fbc9329e20a098000e918e33e1819
skira <ronnsr15@gmail.com>
2022-08-12 14:17:31 -0700

    ./pages/signup.js rename signup function to Signup

--- pages/signup.js
+++ pages/signup.js
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // import { signIn } from 'next-auth/react';
 
-const signup = () => {
-  const [ email, setEmail ] = useState('');
-  const [ password, setPassword ] = useState('');
-  const [ validatePassword, setValidatePassword ] = useState('');
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [validatePassword, setValidatePassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     //Validation
-    if (!email || !email.includes('@') || !password) {
-      alert('Invalid details');
+    if (!email || !email.includes("@") || !password) {
+      alert("Invalid details");
       return;
     }
-    if(password != validatePassword){
-      alert('Passwords do not match');
+    if (password != validatePassword) {
+      alert("Passwords do not match");
       return;
     }
 
     //POST form values
-    const res = await fetch('/api/auth/signup', {
-      method: 'POST',
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email: email,
@@ -35,20 +35,43 @@ const signup = () => {
     console.log(data);
   };
 
-
-
   return (
     <div>
       <h2>signup</h2>
       <form onSubmit={handleSubmit}>
-        <label>Email: <input required type='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='johndoe@email.com' /></label>
-        <label>Password: <input required type='password' value={password} onChange={(e) => setPassword(e.target.value)} /></label>
-        <label>Re-type Password: <input required type='password' value={validatePassword} onChange={(e) => setValidatePassword(e.target.value)} /></label>
+        <label>
+          Email:{" "}
+          <input
+            required
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="johndoe@email.com"
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            required
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label>
+          Re-type Password:{" "}
+          <input
+            required
+            type="password"
+            value={validatePassword}
+            onChange={(e) => setValidatePassword(e.target.value)}
+          />
+        </label>
 
-        <button type='submit'>Signup</button>
+        <button type="submit">Signup</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default signup
+export default Signup;
